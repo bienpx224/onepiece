3cb5a0a81465f67503ab768796d2db7c2150aa65
bienpx224 <bienphamxuan@vccorp.vn>
2021-12-03 11:46:03 +0700

    + client: base ...

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React haha
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {connect} from 'react-redux';
+import {BrowserRouter as Router,Route,Switch,Ridirect,hashHistory,Redirect,NavLink} from 'react-router-dom';
+
+import Home from 'Home'
+var {Provider} = require('react-redux');
+var store = require('./store/store');
+
+
+class App extends React.Component{
+     render(){
+        return(
+
+            <Provider store={store}>
+            <Router history={hashHistory}>
+                  <Layout>
+                   <Switch>
+                      <Route  exact path="/" component={Home}/>
+                      <Route  path="/home" component={Home}/>
+                      <Route render={function(){
+                          return <Home />
+                      } } />
+                    </Switch>
+                  </Layout>
+            </Router>
+            </Provider>
+    )
+  }
 }
 
 export default App;
